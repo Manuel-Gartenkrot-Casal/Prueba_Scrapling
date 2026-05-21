7b381ea9440dfb017d4a15400f36beb99549d972
Manuel-Gartenkrot-Casal <manucasal07@gmail.com>
2026-05-21 19:33:44 -0300

    Borro archivos de pruba

--- listar_modelos.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-from google import genai
-
-load_dotenv()
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-for m in client.models.list():
-    print(m.name)
